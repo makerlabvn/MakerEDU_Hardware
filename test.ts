b278b1a3cedc52da08c67e519d64a437ee20c923
xuanngoc1992 <xuanngoc1992@gmail.com>
2023-02-16 12:55:42 +0700

    add content test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+{
+    lcd.displayText(convertToText(ds18b20.readTemperature(ds18b20.PinKit.P0, ds18b20.TemperatureType.Celsius)), 1, 1)
+    basic.pause(500)
+    lcd.clearScreen()
+}
